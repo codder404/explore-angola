b7e3475142f4452e5ca0254bb871fe96873be773
Fernando dos Santos <nando4803@gmail.com>
2020-09-21 13:32:54 +0100

    ☕ this commit update footer

--- src/pages/galery.js
+++ src/pages/galery.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Layout from '../components/layout';
+import Images from '../components/ImageComp';
 import SEO from '../components/seo';
 
 const Galery = () => (
   <Layout>
     <SEO title="Galeria de fotos" />
-    <h1>Galeria de fotos</h1>
+    <Images />
   </Layout>
 )
 
