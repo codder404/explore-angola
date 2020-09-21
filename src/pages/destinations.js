080f1938f1fa3eb3505751b8021f3d7da6d2503a
Fernando dos Santos <nando4803@gmail.com>
2020-09-21 09:23:27 +0100

    🦄 this commit add destinations page

---
+++ src/pages/destinations.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const Destinations = () => (
+  <Layout>
+    <SEO title="Lugares para visitar" />
+    <h1>Lugares para não perder em Angola</h1>
+  </Layout>
+)
+
+export default Destinations;
