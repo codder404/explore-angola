f3f0894bcd7e7814b9223765d732599c106b091d
Fernando dos Santos <nando4803@gmail.com>
2020-09-21 10:23:03 +0100

    🥳 this commit import hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import Hero from '../components/Hero';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hello world!</h1>
+    <Hero />
   </Layout>
 )
 
