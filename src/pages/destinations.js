8e3581243ce18833ac8f0b8f5b91a58a03d34d94
Fernando dos Santos <nando4803@gmail.com>
2020-09-21 13:40:40 +0100

    🦄 this commit import destinations

--- src/pages/destinations.js
+++ src/pages/destinations.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Layout from '../components/layout';
+import Destination from '../components/Destinations';
 import SEO from '../components/seo';
 
 const Destinations = () => (
   <Layout>
     <SEO title="Lugares para visitar" />
-    <h1>Lugares para não perder em Angola</h1>
+   <Destination />
   </Layout>
 )
 
