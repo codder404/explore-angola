022d271cfde1bab4eb0d35bd0ee50848059c204e
Fernando dos Santos <nando4803@gmail.com>
2020-09-21 08:35:40 +0100

    🦄 this commit add navbar content

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+
+import * as S from './styles';
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const [scroll, setScroll] = useState(false);
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
+  }
+
+  useEffect(() => {
+    changeNav()
+    window.addEventListener("scroll", changeNav);
+  }, []);
+
   return (
-    <h1>Navbar</h1>
+    <>
+      <IconContext.Provider value={{color: '#141414'}}>
+        <S.Navbar active={scroll} click={click}>
+          <S.NavbarContainer>
+            <S.NavbarLogo to="/">
+              <S.NavbarIcon />
+              Explore Angola
+            </S.NavbarLogo>
+          </S.NavbarContainer>
+        </S.Navbar>
+      </IconContext.Provider>
+   </>
   )
 }
 
