43626a87517d0e0635970ae1af99e6518fbd3ec9
Fernando dos Santos <nando4803@gmail.com>
2020-09-21 10:22:06 +0100

    ☕ this commit update hero

--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
-import backgroundImg from '../../assets/';
 
 export const Hero = styled.section`
+  background-image: url("https://images.unsplash.com/photo-1543163181-25b5600dabb9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80") center center/cover no-repeat;
   
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  box-shadow: inset 0 0 0 1000px rgba(0, 0, 0, 0.2);
+  object-fit: contain;
+  margin-top: -80px;
 `;
 
 export const HeroTitle = styled.h1``;
