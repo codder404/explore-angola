d12e546df5fdb6e4d527bbe3e7bd56d488767ee9
Fernando dos Santos <nando4803@gmail.com>
2020-09-21 12:25:23 +0100

    ☕ this commit update images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Hero from '../components/Hero';
+import Image from '../components/ImageComp';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
@@ -8,6 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
+    <Image />
   </Layout>
 )
 
